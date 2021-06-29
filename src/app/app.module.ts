fc94daab41c7fa832dd2730d446694c3e0dcfcf1
snehalupatil <patilsnehal4575@gmail.com>
2021-06-29 21:20:27 +0530

    Added: Registartion folder is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { RegistartionComponent } from './pages/registration/registartion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistartionComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/pages/registration/registartion.component.spec.ts
+++ src/app/pages/registration/registartion.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegistrationComponent } from './registration.component';
+import { RegistartionComponent } from './registartion.component';
 
-describe('RegistrationComponent', () => {
-  let component: RegistrationComponent;
-  let fixture: ComponentFixture<RegistrationComponent>;
+describe('RegistartionComponent', () => {
+  let component: RegistartionComponent;
+  let fixture: ComponentFixture<RegistartionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ]
+      declarations: [ RegistartionComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegistrationComponent);
+    fixture = TestBed.createComponent(RegistartionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
