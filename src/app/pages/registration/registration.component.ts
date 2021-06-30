59f055fc38bb07acc16f2c00e834c27fe5837b95
snehalupatil <patilsnehal4575@gmail.com>
2021-06-30 19:07:23 +0530

    Added: Validations added

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -9,7 +10,28 @@ export class RegistrationComponent implements OnInit {
 
   constructor() { }
 
+  form = new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(1)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(1)])
+  })
+
+  hide: Boolean = false
+
   ngOnInit(): void {
   }
 
+  get f() {
+    return this.form.controls;
+  }
+
+  
+
+  submit() {
+    console.log(this.form);
+  }
+
 }
+
+
